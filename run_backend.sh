3f2c0bba63b753585f4468bc8777d2e00201013e
Luka <lukarod@icloud.com>
2025-09-24 16:45:10 -0400

    Added mac/linux commands to free up ports I wanted to use for both backend and frontend. Demo should work now

--- run_backend.sh
+++ run_backend.sh
@@ -10,6 +10,12 @@ mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . -j
 
+# Free the port (macOS/Linux) - OPTIONAL
+if lsof -ti :8080 >/dev/null 2>&1; then
+  echo "Killing process on :8080..."
+  kill -9 $(lsof -ti :8080)
+fi
+
 
 # Adding message that displays backend local port
 echo "Starting backend on http://127.0.0.1:8080 ..."
--- run_frontend.sh
+++ run_frontend.sh
@@ -6,6 +6,12 @@ cd "$ROOT/frontend"
 
 command -v npm >/dev/null 2>&1 || { echo "npm not found. Install Node.js (>=18)."; exit 1; }
 
+# Free the port (macOS/Linux)
+if lsof -ti :5173 >/dev/null 2>&1; then
+  echo "Killing process on :5173..."
+  kill -9 $(lsof -ti :5173)
+fi
+
 npm install
 echo "Starting frontend on http://127.0.0.1:5173 ..."
 exec npm run dev
